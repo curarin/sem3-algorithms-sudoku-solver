3e0666f9bf95a8d3f6a4a32c82d3f7db29d25013
Paul Mitsch <paul.herzog@urlaubsguru.de>
2026-09-09 10:56:43 +0200

    mvp

--- src/main/java/at/hochschule/burgenland/bswe/algo/Solver.java
+++ src/main/java/at/hochschule/burgenland/bswe/algo/Solver.java
@@ -3,6 +3,7 @@ package at.hochschule.burgenland.bswe.algo;
 public class Solver {
     public static int[][] solve(int[][] array) {
         Sudoku sudoku = Sudoku.instanceOf(array);
+
         do {
             sudoku.solve();
 
